ebfeea284781e9e670f1adda51cfc690c0c28210
moonthien <letrongthien23032003@gmail.com>
2024-10-05 17:50:09 +0700

    xong tuan 4

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Cau2a from './screens/Cau2a';
+import Cau2b from './screens/Cau2b';
+import Cau2c from './screens/Cau2c';
+import Tiki_Ok from './screens/TikTokOK';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+   
+     {/* <Cau2a /> 
+     <Cau2b /> */}
+     <Cau2c />
+    {/* <Tiki_Ok /> */}
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ constants/images.js
@@ -0,0 +1,14 @@
+export const images = {
+    Lock: require('../assets/lock.png'),
+    Eye: require('../assets/eye.png'),
+    AvatarUser: require('../assets/avatar_user.png'),
+
+    USB: require('../assets/usb.png'),
+    Start: require('../assets/Star.png'),
+    Camera: require('../assets/camera.png'),
+    Tick: require('../assets/tick.png'),
+    NotTick: require('../assets/notTick.png'),
+
+    Book: require('../assets/book.png'),
+    YellowBlock: require('../assets/yellow_block.png'),
+};
